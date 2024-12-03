d2cab05ae7b818941dbeadf330221035bb164888
MatteoCasu <ra-9817@student.iram.be>
2024-12-03 08:39:24 +0100

    Ajout de tous les boutons

--- src/main/java/be/iramps/ue1396/gui/App.java
+++ src/main/java/be/iramps/ue1396/gui/App.java
@@ -20,9 +20,12 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        Insets insets = new Insets(10, 10, 10, 10);
         VBox vBox = new VBox(20);
+
         TextField tFScreen = new TextField("Entrez Chiffre");
         tFScreen.setDisable(true);
+
         HBox hBox789 = new HBox(20);
         Button b7 = new Button("7");
         Button b8 = new Button("8");
@@ -33,10 +36,46 @@ public class App extends Application {
         b9.setMinSize(40, 40);
         bPlus.setMinSize(40, 40);
         hBox789.getChildren().addAll(b7, b8, b9, bPlus);
-        Insets insets = new Insets(20, 20, 20, 20);
         hBox789.setPadding(insets);
-        vBox.getChildren().addAll(tFScreen, hBox789);
-        scene = new Scene(vBox, 260, 220);
+
+        HBox hBox456 = new HBox(20);
+        Button b4 = new Button("4");
+        Button b5 = new Button("5");
+        Button b6 = new Button("6");
+        Button bMoins = new Button("-");
+        b4.setMinSize(40, 40);
+        b5.setMinSize(40, 40);
+        b6.setMinSize(40, 40);
+        bMoins.setMinSize(40, 40);
+        hBox456.getChildren().addAll(b4, b5, b6, bMoins);
+        hBox456.setPadding(insets);
+
+        HBox hBox123 = new HBox(20);
+        Button b1 = new Button("1");
+        Button b2 = new Button("2");
+        Button b3 = new Button("3");
+        Button bMult = new Button("*");
+        b1.setMinSize(40, 40);
+        b2.setMinSize(40, 40);
+        b3.setMinSize(40, 40);
+        bMult.setMinSize(40, 40);
+        hBox123.getChildren().addAll(b1, b2, b3, bMult);
+        hBox123.setPadding(insets);
+
+        HBox hBox0 = new HBox(20);
+        Button bPoint = new Button(".");
+        Button b0 = new Button("0");
+        Button bEgale = new Button("=");
+        Button bCE = new Button("CE");
+        bPoint.setMinSize(40, 40);
+        b0.setMinSize(40, 40);
+        bEgale.setMinSize(40, 40);
+        bCE.setMinSize(40, 40);
+        hBox0.getChildren().addAll(bPoint, b0, bEgale, bCE);
+        hBox0.setPadding(insets);
+
+        vBox.getChildren().addAll(tFScreen, hBox789, hBox456, hBox123, hBox0);
+        scene = new Scene(vBox, 240, 360);
         stage.setScene(scene);
         stage.show();
     }
